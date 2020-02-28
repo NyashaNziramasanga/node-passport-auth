993880923d4a92b648b59741aa31e5854f2335eb
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-02-28 22:12:11 +1030

    Added users endpoints

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 
 // Routes
 app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 const PORT = process.envPORT || 5000;
 app.listen(PORT, console.log(`🔥 Server started on port ${PORT}🔥`));
