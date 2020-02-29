55ea4a7ddf4def7b3745a7fec138270669523ea8
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-03-01 00:20:58 +1030

    success message
    - Server session for success msg
    - Flash and session middleware

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const colors = require('colors');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
@@ -20,6 +22,26 @@ app.set('view engine', 'ejs');
 
 // Bodyparser
 app.use(express.urlencoded({ extended: false }));
+
+// Express session
+app.use(
+	session({
+		secret: 'secret',
+		resave: true,
+		saveUninitialized: true,
+		cookie: { secure: true }
+	})
+);
+
+// Connect Flash
+app.use(flash());
+
+// Global vars
+app.use((req, res, next) => {
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+	next();
+});
 
 // Routes
 app.use('/', require('./routes/index'));
