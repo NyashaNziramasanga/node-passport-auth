0f1b43a82317c15afb519d64d3a29d10db9a4272
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-02-29 23:07:28 +1030

    Password hasing and error message
    - Hashed password to mongodb
    - Error message on validation

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+// User model
+const User = require('../models/User');
 
 // Login page
 router.get('/login', (req, res) => res.render('login'));
@@ -42,7 +46,43 @@ router.post('/register', (req, res) => {
 			password2
 		});
 	} else {
-		res.send('Pass ');
+		// Validation passed
+		User.findOne({
+			email: email
+		}).then(user => {
+			if (user) {
+				// User exists
+				errors.push({ msg: 'Email is already registered' });
+				res.render('register', {
+					errors,
+					name,
+					email,
+					password,
+					password2
+				});
+			} else {
+				const newUser = new User({
+					name,
+					email,
+					password
+				});
+				// Hash password
+				bcrypt.genSalt(10, (err, salt) => {
+					bcrypt.hash(newUser.password, salt, (err, hash) => {
+						if (err) throw err;
+						// Set password to hashed
+						newUser.password = hash;
+						// Save user
+						newUser
+							.save()
+							.then(user => {
+								res.redirect('/users/login');
+							})
+							.catch(err => console.log(err));
+					});
+				});
+			}
+		});
 	}
 });
 
